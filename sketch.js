7b3df29fa5517c9f4fbd453542dcff8c6fe98146
htien <mac@Macs-MacBook-Pro.local>
2021-05-17 14:36:28 +0700

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,10 +26,14 @@ function draw() {
     for (var x = 0; x < WIDTH; x++) {
       var index = (x + y * WIDTH) * 4;
       var rand = noise(xoff, yoff) * 255;
-      pixels[index + 0] = rand;     //change this value to x, y or anything to get the different colors
-      pixels[index + 1] = rand;     //change this value to x, y or anything to get the different colors
-      pixels[index + 2] = rand;     //change this value to x, y or anything to get the different colors
-      pixels[index + 3] = 255;      //change this value to x, y or anything to get the different colors
+      var a = x % 255;
+      var b = y % 255;
+      b = b % 2 ? 255 - b : b;      // some values to play with
+      a = a % 2 ? 255 - a : a;      //some values to play with
+      pixels[index + 0] = rand;     //change this value to x, y, a, b or anything to get different colors
+      pixels[index + 1] = rand;     //change this value to x, y, a, b or anything to get different colors
+      pixels[index + 2] = rand;     //change this value to x, y, a, b or anything to get different colors
+      pixels[index + 3] = 255;      //change this value to x, y, a, b or anything to get different colors
 
       xoff += inc;
     }
